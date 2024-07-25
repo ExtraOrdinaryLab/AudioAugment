beef3d584ebf06049c2fc09d45a623fc9cfb89e2
Yang Wang <penguinwang@smail.nchu.edu.tw>
2024-07-25 21:51:00 +0100

    [feat] add Reverb

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='audio_augment', 
-    version='0.0.1', 
+    version='0.0.2', 
     description='Open-source library of AudioAugment', 
     author='Yang Wang', 
     author_email='yangwang4work@gmail.com', 
