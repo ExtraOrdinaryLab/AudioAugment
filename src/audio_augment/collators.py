bc8917cfbebb18c5be3b8c238641ff3c132a205c
Yang Wang <penguinwang@smail.nchu.edu.tw>
2024-07-25 12:07:53 +0100

    [refactor] remove normalize

--- src/audio_augment/collators.py
+++ src/audio_augment/collators.py
@@ -12,19 +12,14 @@ class HuggingFaceAudioDatasetCollator(object):
 
     def __init__(
         self, 
-        sampling_rate: int = 16000, 
         do_normalize: bool = True, 
         mean: float = -4.2677393, 
         std: float = 4.5689974, 
     ):
-        self.sampling_rate = sampling_rate
         self.do_normalize = do_normalize
         self.mean = mean
         self.std = std
 
-    def _normalize(self, input_values: np.ndarray) -> np.ndarray:
-        return (input_values - (self.mean)) / (self.std * 2)
-    
     def __call__(
         self, 
         examples, 
@@ -36,8 +31,6 @@ class HuggingFaceAudioDatasetCollator(object):
         input_values = padded_inputs.get("input_values")
         if isinstance(input_values[0], list):
             padded_inputs["input_values"] = [np.asarray(feature, dtype=np.float32) for feature in input_values]
-        if self.do_normalize:
-            padded_inputs["input_values"] = [self._normalize(feature) for feature in input_values]
         if return_tensors is not None:
             padded_inputs = padded_inputs.convert_to_tensors(return_tensors)
         padded_inputs['labels'] = torch.vstack(
