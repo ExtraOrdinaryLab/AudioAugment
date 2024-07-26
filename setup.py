94f8717db271e0e40600fc89e871b78fe00fa6dd
Yang Wang <penguinwang@smail.nchu.edu.tw>
2024-07-26 15:27:12 +0100

    [fix] git dependecies

--- setup.py
+++ setup.py
@@ -12,16 +12,20 @@ def parse_requirements(filename):
 # Read requirements from requirements.txt
 requirements = parse_requirements('requirements.txt')
 
+# Separate Git dependencies from PyPI dependencies
+install_requires = [req for req in requirements if not req.startswith('git+')]
+dependency_links = [req for req in requirements if req.startswith('git+')]
 
 setup(
     name='audio_augment', 
-    version='0.0.6', 
+    version='0.0.7', 
     description='Open-source library of AudioAugment', 
     author='Yang Wang', 
     author_email='yangwang4work@gmail.com', 
     package_dir={'': 'src'}, 
     packages=find_packages('src'), 
-    install_requires=requirements, 
+    install_requires=install_requires,  # Use the PyPI dependencies from requirements.txt
+    dependency_links=dependency_links,  # Use the Git dependencies from requirements.txt
     python_requires='>=3.9.0', 
     keywords='audio augment augmentation'
 )
